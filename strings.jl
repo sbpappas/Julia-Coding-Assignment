b0d147e3bf5abb5c0389f145af2452b4ef6548c5
Samuel Pappas <spappas@trinity.edu>
2024-03-09 17:30:35 -0600

    got addition to excel sheet to work

--- strings.jl
+++ strings.jl
@@ -4,12 +4,17 @@ using Pkg
 Pkg.add("XLSX") 
 import XLSX 
 xf = XLSX.readxlsx("infoTable.xlsx")
+sh = xf["Sheet1"] 
+sh[:]
 
+information = String[]
 print("Welcome to Trinity University. Please enter your first and last name.\n")
 name = readline()
 name = split(name, " ") #shadows
 first = name[1] 
 surname = name[2]
+push!(information, first)
+push!(information, surname)
 
 print("What is your phone number? \nPlease enter it in this format: ###-###-#### \n")
 phone = readline()
@@ -25,20 +30,35 @@ if isValidPhoneNumber(phone) == false
         if isValidPhoneNumber(phone) == false
             print("OOPS! \nPlease enter your number it in THIS format: ###-###-#### \n")
             phone = readline()
-        else keepAsking == false
+        else 
+            keepAsking == false
+            print(phone)
+            #didn't work having this here: push!(information, phone)
         end
     end
 end
+push!(information, phone)
 
-newRow = Dict("First Name" => first, "Last Name" => surname, "Phone Number" => phone)
-#=sheet_name = keys(xf.sheets)[1]
+XLSX.openxlsx("infoTable.xlsx", mode="rw") do xf 
+    sheet = xf[1] 
+    sheet["A1"] = "First Name"  
+    sheet["B1"] = "Last Name"  
+    sheet["C1"] = "Phone Number" 
+    letter = "F"
+    for i in 1:length(information)
+        if i%3 == 0
+            letter = "C"
+        elseif (i-1)%3 == 0
+            letter = "A"
+        else 
+            letter = "B"
+        end
+        #map index to letter
+        #convert i to string
+        j = string(i)
+        spot = letter*"2"
+        print(information)
+        sheet[spot] = information[i]
+    end
+end
 
-# Append the new row to the existing data
-existing_data = DataFrame(XLSX.readtable("infoTable.xlsx", sheet_name)..., copycols=true)
-new_data = vcat(existing_data, DataFrame([newRow]))
-
-# Write the updated data back to the Excel file
-XLSX.writetable("infoTable.xlsx", Dict(sheet_name => new_data), anchor="A1")
-
-println("Your information has been added to the Excel sheet.")
-=#
